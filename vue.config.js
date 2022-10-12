5372f7606345bbd6d15f42d6e47434403999b97e
wangmin <wangmin@bixin.cn>
2022-10-12 22:56:16 +0800

    feat: build

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // 选项...
+  lintOnSave: false,
+  publicPath: '/websocket-chatroom/dist',
+  outputDir: 'dist',
+  // publicPath: '/vue-demos',
+  // outputDir: 'vue-demos',
+}
